db8e370c4e4c4dbfd1af20ff69b182c11409220d
EC2 Default User <ec2-user@ip-172-31-92-241.ec2.internal>
2021-10-22 14:57:07 +0000

    firstmovie finished

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e07f34c8b8eff51186daac32c43181505cc91c74e3cfa09d34edc5485446d32cd7b2372cf70a202c9700dbc873208e6b229109c5af29bdeb26483ce0e9378f2a'
